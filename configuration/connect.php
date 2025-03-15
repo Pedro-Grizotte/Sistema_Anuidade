b1f5e2f0acd775a1e6d30e39838a6feb50b1cebc
Pedro Grizotte <pgrizotte@gmail.com>
2025-03-14 22:41:29 -0300

    Criando conexão com o Banco de Dados

---
+++ configuration/connect.php
@@ -0,0 +1,31 @@
+<?php
+    define('HOST', 'localhost:3306');
+    define('DATABASENAME', 'Anuidade');
+    define('USER', 'root');
+    define('PASSWORD', 'boreal11');
+
+    class Connect {
+        private $connection;
+        private static $instance = null;
+        function __construct() {
+            $this->connectDatabase(); 
+        }
+        function connectDatabase() {
+            try {
+                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DATABASENAME, USER, PASSWORD);
+            } catch (PDOException $e) {
+                echo 'Error: ' . $e->getMessage();
+                die();
+            }
+        }
+        public function getInstance() {
+            if(self::$instance == null) {
+                self::$instance = new Connect();
+            }
+            return self::$instance;
+        }
+        public function getConnection() {
+            return $this->connection;
+        }
+    }
+?>
---
+++ meu_database.sql
@@ -0,0 +1,6 @@
+/* Arquivo criado para meus comandos mySql */
+
+create database Anuidade;
+
+use Anuidade;
+
